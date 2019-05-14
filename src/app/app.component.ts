7a5a851a23f9545c2003975ef672f446bef21d90
PatouBC <pbergez@outlook.fr>
2019-05-14 15:58:24 +0200

    ready for bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'toto';
-}
-export class SidenavOpenCloseExample {
-  events: string[] = [];
+  title = 'My Application';
   opened: boolean;
 
   shouldRun = true;
